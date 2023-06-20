cbece0daf5374878360c5f133c61ab3d3eee06e5
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-06-20 16:25:13 +0200

    (state): display list of movies from json file + functionality to delete

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,40 @@
+import { useState } from "react";
+import movies from "../data/movies.json";
+
+import "./Main.css";
+
 function Main() {
+
+
+    const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+    
+    const deleteMovie = (movieId) => {
+        console.log("deleting movie with id....", movieId)
+
+        // moviesToDisplay.push(); // NEVER MODIFY STATE DIRECTLY !
+
+        const newList = moviesToDisplay.filter( (element) => {
+            return element.id !== movieId;
+        });
+
+        setMoviesToDisplay(newList);
+
+    }
+
     return (
-        <h1>This is the Main component</h1>
+        <div className="Main">
+
+            <h1>Number of movies: {moviesToDisplay.length}</h1>
+
+            {moviesToDisplay.map((movieObj) => {
+                return(
+                    <div key={movieObj.id} className="card">
+                        <p>{movieObj.title}</p>
+                        <button onClick={ () => {deleteMovie(movieObj.id)}}>Delete this movie</button>
+                    </div>
+                )
+            })}
+        </div>
     );
 }
 
